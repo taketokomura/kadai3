2a304643e6bcbb429245f82ddeff1b4efc5d21c1
taketokomura <s19c1045NK@s.chibakoudai.jp>
2021-01-08 02:39:37 +0900

    Add a file

---
+++ kada3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int n, x, i;
+	printf("整数を入力:");
+	scanf("%d", &n);
+	printf("%d", n);
+	for (i=2, x=n; i<=sqrt(n); )
+	{
+		if (n%i == 0)
+		{
+			printf(" 素数:%d", i);
+			n=n/i;
+			x=n;
+		}
+		else
+		{
+			if(i>2)
+			{
+				i++;
+			}
+		i++;
+		}
+	}
+	printf(" 素数:%d\n", x);
+	return 0;
+}
+
